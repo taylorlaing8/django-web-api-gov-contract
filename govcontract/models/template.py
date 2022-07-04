f0f7abc1871de7548a4437a340c7ed340eb1686d
Taylor Laing <taylorlaing8@gmail.com>
2022-07-04 14:47:14 -0600

    rename subtitle to sub_title

--- govcontract/models/template.py
+++ govcontract/models/template.py
@@ -3,7 +3,7 @@ from django.db import models
 class Template(models.Model):
     id          = models.AutoField(primary_key=True)
     title       = models.CharField(max_length=255)
-    subtitle    = models.CharField(max_length=255, null=True)
+    sub_title   = models.CharField(max_length=255, null=True)
     data        = models.JSONField()
     created     = models.DateTimeField(auto_now_add=True)
 
--- govcontract/serializers/template.py
+++ govcontract/serializers/template.py
@@ -4,7 +4,7 @@ from ..models import Template
 
 class TemplateSerializer(serializers.ModelSerializer):
     title       = serializers.CharField(max_length=255)
-    subtitle    = serializers.CharField(max_length=255, allow_null=True)
+    sub_title   = serializers.CharField(max_length=255, allow_null=True)
     data        = serializers.JSONField()
 
     class Meta:
