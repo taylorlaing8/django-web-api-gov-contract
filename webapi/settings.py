b1ead596fc4dc1df4308f81b6117db7ae5e78835
Taylor Laing <taylorlaing8@gmail.com>
2024-10-05 15:49:33 -0600

    use env key

--- webapi/settings.py
+++ webapi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x33!hh=njz04p=vm)q=yyh5zf*289$v72vsy(=f0joezq3kq_a"
+SECRET_KEY = os.environ["EB_AWS_ACCESS_KEY_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
